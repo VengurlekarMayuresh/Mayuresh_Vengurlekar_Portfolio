0275e40c21831179a9ebc99cd6c381007438764b
Mayuresh Vengurlekar <onlinemayuresh29@gmail.com>
2025-07-05 00:05:22 +0530

    Initial commit

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,7 @@ export default defineConfig({
   plugins: [react(),tailwindcss()],
   server: {
     host: '0.0.0.0',
-    port: process.env.PORT || 3000
+    port: process.env.PORT || 3000,
+    allowedHosts: ['mayuresh-vengurlekar-portfolio-1.onrender.com']
   }
 })
