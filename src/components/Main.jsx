2cb74c77064f6457a2b4a8c4603da5d9e21823ad
Mayuresh Vengurlekar <onlinemayuresh29@gmail.com>
2025-07-05 02:10:13 +0530

    Initial commit

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -8,7 +8,7 @@ import "animate.css";
 export default function Main() {
   
   return (
-    <div className="flex lg:flex-row h-[90vh] flex-col lg:items-start items-center justify-center gap-5 lg:gap-20 p-5 lg:pt-30 overflow-x-hidden relative">
+    <div className="flex lg:flex-row h-[95vh] md:h-[100vh] flex-col lg:items-start items-center justify-center gap-5 lg:gap-20 p-5 lg:pt-30 overflow-x-hidden relative">
       
       {/* LEFT CONTENT */}
       <motion.div
